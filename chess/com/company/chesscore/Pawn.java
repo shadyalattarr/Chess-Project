876635e71af7cbc9537d7ca254d6fb01db00e8b4
Abdelrahman mohamed <108292349+Gumbile@users.noreply.github.com>
2023-11-21 23:10:34 +0200

    Update Pawn.java

--- chess/com/company/chesscore/Pawn.java
+++ chess/com/company/chesscore/Pawn.java
@@ -3,13 +3,11 @@ package com.company.chesscore;
 import java.util.ArrayList;
 
 public class Pawn extends Piece {
- 
 
-    public Pawn(int color,Board board) {
+    public Pawn(int color, Board board) {
         setColor(color);
         this.board = board;
     }
-    
 
     // need to create a get all valid moves method for each piece
     public boolean isValidMove(int myPosition, int nextPostion) {
@@ -19,11 +17,11 @@ public class Pawn extends Piece {
         int nextCol = nextPostion % 8;
         int rowDifference = myRow - nextRow;
         int colDifference = myCol - nextCol;
-       
+
         if (getColor().equals("Black")) {
             if (firstMove) {
-                //can go down 2 or 1 or diags
-                //nn check isillegal cuz first move
+                // can go down 2 or 1 or diags
+                // nn check isillegal cuz first move
                 if (((rowDifference == -2 && colDifference == 0)
                         && (board.getBoardSquare(nextPostion - 8).getPiece().getColor().equals("Empty Square")))
                         || ((rowDifference == -1 && colDifference == 0))) {
@@ -31,7 +29,7 @@ public class Pawn extends Piece {
                     return true;
                 }
             }
-            //not first move
+            // not first move
             if ((rowDifference == -1 && colDifference == 0)) {
                 return true;
             } else if ((rowDifference == -1 && colDifference == 1)
@@ -82,20 +80,20 @@ public class Pawn extends Piece {
         int rowDifference = myRow - nextRow;
         BoardSquare nextBoardSquare = board.getBoardSquare(nextPostion);
         if (colDifference == 0) {
-            //fwd
+            // fwd
             if (nextBoardSquare.getPiece().getColor().equals("Empty Square"))// it can move only when its empty
-                if (rowDifference > 1) {//going down.. a black pawn fwd
+                if (rowDifference > 1) {// going down.. a black pawn fwd
                     if (board.getBoardSquare(nextPostion + 8).getPiece().getColor().equals("Empty Square"))
-                        return true;//black pawn move fwd 2
+                        return true;// black pawn move fwd 2
                 } else if (rowDifference < -1) {
                     if (board.getBoardSquare(nextPostion - 8).getPiece().getColor().equals("Empty Square"))
-                        return true;//pawn move fwd 2 as white
+                        return true;// pawn move fwd 2 as white
                 } else
-                    return true;//pawn move fwd 1
+                    return true;// pawn move fwd 1
             else
                 return false;
 
-        } else if(colDifference == 1 || colDifference == -1){//diags to eat
+        } else if (colDifference == 1 || colDifference == -1) {// diags to eat
             if (nextBoardSquare.getPiece().getColor().equals(this.getColor())
                     || nextBoardSquare.getPiece().getColor().equals("Empty Square"))// if same color piece or empty
                                                                                     // square
@@ -114,11 +112,11 @@ public class Pawn extends Piece {
         return "P";
     }
 
-    public boolean checkDiagsCapture(int nextPosition)
-    {
-        
+    public boolean checkDiagsCapture(int nextPosition) {
+
         return false;
     }
+
     // fall in last col and first col
     @Override
     public ArrayList<String> getAllValidMovesFromPiece() {
@@ -126,50 +124,64 @@ public class Pawn extends Piece {
         int r = myPosition / 8;
         int c = myPosition % 8;
         if (firstMove) {
-            //nn check illegal
-            if (getColorNum() == 0) {//blck can go down 1 or 2 or diags eat
-                if (board.getBoardSquare(myPosition + 8).getPiece().getColor().equals("Empty Square")) {
-                    if (board.getBoardSquare(myPosition + 16).getPiece().getColor().equals("Empty Square"))
-                        validMoves.add(board.createMoveString(r, c, r + 2, c));//2 fwd
-                    validMoves.add(board.createMoveString(r, c, r + 1, c));//1 fwd
+            // nn check illegal
+            if (getColorNum() == 0) {// blck can go down 1 or 2 or diags eat
+                if (board.getBoardSquare(myPosition + 8).getPiece().getColor().equals("Empty Square")
+                        && isKingSafeFromMyMove(myPosition, myPosition + 8)) {
+                    if (board.getBoardSquare(myPosition + 16).getPiece().getColor().equals("Empty Square")
+                            && isKingSafeFromMyMove(myPosition, myPosition + 16))
+                        validMoves.add(board.createMoveString(r, c, r + 2, c));// 2 fwd
+                    validMoves.add(board.createMoveString(r, c, r + 1, c));// 1 fwd
                 }
-                if (board.getBoardSquare(myPosition + 7).getPiece().getColor().equals("White") && (c > 0))//checkin left diag except 0
+                if (board.getBoardSquare(myPosition + 7).getPiece().getColor().equals("White") && (c > 0)
+                        && isKingSafeFromMyMove(myPosition, myPosition + 7))// checkin left diag except 0
                     validMoves.add(board.createMoveString(r, c, r + 1, c - 1));
-                if (board.getBoardSquare(myPosition + 9).getPiece().getColor().equals("White") && (c < 7))//checkig right diag excet 7
+                if (board.getBoardSquare(myPosition + 9).getPiece().getColor().equals("White") && (c < 7)
+                        && isKingSafeFromMyMove(myPosition, myPosition + 9))// checkig right diag excet 7
                     validMoves.add(board.createMoveString(r, c, r + 1, c + 1));
             } else {
-                //wjite first move go up
-                if (board.getBoardSquare(myPosition - 8).getPiece().getColor().equals("Empty Square")) {
+                // wjite first move go up
+                if (board.getBoardSquare(myPosition - 8).getPiece().getColor().equals("Empty Square")
+                        && isKingSafeFromMyMove(myPosition, myPosition - 8)) {
                     {
 
-                        if (board.getBoardSquare(myPosition - 16).getPiece().getColor().equals("Empty Square"))
+                        if (board.getBoardSquare(myPosition - 16).getPiece().getColor().equals("Empty Square")
+                                && isKingSafeFromMyMove(myPosition, myPosition - 16))
                             validMoves.add(board.createMoveString(r, c, r - 2, c));
                     }
                     validMoves.add(board.createMoveString(r, c, r - 1, c));
-                    if (board.getBoardSquare(myPosition - 7).getPiece().getColor().equals("Black") && (c < 7))
+                    if (board.getBoardSquare(myPosition - 7).getPiece().getColor().equals("Black") && (c < 7)
+                            && isKingSafeFromMyMove(myPosition, myPosition - 7))
                         validMoves.add(board.createMoveString(r, c, r - 1, c + 1));
-                    if (board.getBoardSquare(myPosition - 9).getPiece().getColor().equals("Black") && (c > 0))
+                    if (board.getBoardSquare(myPosition - 9).getPiece().getColor().equals("Black") && (c > 0)
+                            && isKingSafeFromMyMove(myPosition, myPosition - 9))
                         validMoves.add(board.createMoveString(r, c, r - 1, c - 1));
                 }
 
             }
-        } else {//not first move
-            if (getColorNum() == 0 && (r < 7)) {//black and not in end// if in end pawn promo
-                //can have pawn promo here-- eads to it
-                if (board.getBoardSquare(myPosition + 8).getPiece().getColor().equals("Empty Square")) {
+        } else {// not first move
+            if (getColorNum() == 0 && (r < 7)) {// black and not in end// if in end pawn promo
+                // can have pawn promo here-- eads to it
+                if (board.getBoardSquare(myPosition + 8).getPiece().getColor().equals("Empty Square")
+                        && isKingSafeFromMyMove(myPosition, myPosition + 8)) {
                     validMoves.add(board.createMoveString(r, c, r + 1, c));
                 }
-                if (board.getBoardSquare(myPosition + 7).getPiece().getColor().equals("White") && (c > 0))
+                if (board.getBoardSquare(myPosition + 7).getPiece().getColor().equals("White") && (c > 0)
+                        && isKingSafeFromMyMove(myPosition, myPosition + 7))
                     validMoves.add(board.createMoveString(r, c, r + 1, c - 1));
-                if (board.getBoardSquare(myPosition + 9).getPiece().getColor().equals("White") && (c < 7))
+                if (board.getBoardSquare(myPosition + 9).getPiece().getColor().equals("White") && (c < 7)
+                        && isKingSafeFromMyMove(myPosition, myPosition + 9))
                     validMoves.add(board.createMoveString(r, c, r + 1, c + 1));
-            } else {//will neer have apawn in black and at bottom
+            } else {// will neer have apawn in black and at bottom
                 if (r > 0) {
-                    if (board.getBoardSquare(myPosition - 8).getPiece().getColor().equals("Empty Square"))
+                    if (board.getBoardSquare(myPosition - 8).getPiece().getColor().equals("Empty Square")
+                            && isKingSafeFromMyMove(myPosition, myPosition - 8))
                         validMoves.add(board.createMoveString(r, c, r - 1, c));
-                    if (board.getBoardSquare(myPosition - 7).getPiece().getColor().equals("Black") && (c < 7))
+                    if (board.getBoardSquare(myPosition - 7).getPiece().getColor().equals("Black") && (c < 7)
+                            && isKingSafeFromMyMove(myPosition, myPosition - 7))
                         validMoves.add(board.createMoveString(r, c, r - 1, c + 1));
-                    if (board.getBoardSquare(myPosition - 9).getPiece().getColor().equals("Black") && (c > 0))
+                    if (board.getBoardSquare(myPosition - 9).getPiece().getColor().equals("Black") && (c > 0)
+                            && isKingSafeFromMyMove(myPosition, myPosition - 9))
                         validMoves.add(board.createMoveString(r, c, r - 1, c - 1));
 
                 }
@@ -177,7 +189,6 @@ public class Pawn extends Piece {
         }
         return validMoves;
     }
-
 
     public void setFirstMove(boolean firstMove) {
         this.firstMove = firstMove;
